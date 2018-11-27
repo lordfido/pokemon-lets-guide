cb856482f054bedb5034f299165f43fe1ba28844
Rafael Pérez <rafael.perez@imperdiblesoft.com>
2018-11-27 19:11:47 +0100

    Home button is working :)

--- src/app/app-view.tsx
+++ src/app/app-view.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react';
 import classnames from 'classnames';
+import { Link } from 'react-router-dom';
 import { isInstalledPWA } from '../common/utils/platforms';
 
 import HeaderWrapper from './shell/header/header-wrapper';
 import FooterWrapper from './shell/footer/footer-wrapper';
+
+import { HOME } from '../constants/appRoutes';
 
 class AppView extends React.Component {
   static displayName = 'AppView';
@@ -12,16 +15,19 @@ class AppView extends React.Component {
     const { children } = this.props;
 
     return (
-      <div
-        id="app"
-        className={classnames('App', {
-          'is-pwa': isInstalledPWA(),
-        })}
-      >
-        <HeaderWrapper />
-        <div className="App-content">{children}</div>
-        <FooterWrapper />
-      </div>
+      <React.Fragment>
+        <Link className="HomeButton" to={{ pathname: HOME }} />
+        <div
+          id="app"
+          className={classnames('App', {
+            'is-pwa': isInstalledPWA(),
+          })}
+        >
+          <HeaderWrapper />
+          <div className="App-content">{children}</div>
+          <FooterWrapper />
+        </div>
+      </React.Fragment>
     );
   }
 }
