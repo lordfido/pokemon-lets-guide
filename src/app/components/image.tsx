2db847d5a596e512ca9aa556b2fac9383e885769
Rafael Pérez <rafael.perez@imperdiblesoft.com>
2018-11-27 18:40:53 +0100

    Using react-lazy-img, some improvements

--- src/app/components/image.tsx
+++ src/app/components/image.tsx
@@ -1,58 +1,18 @@
 import * as React from 'react';
+import Img from 'react-lazy-img';
 
 interface OwnProps {
   src: string;
   [key: string]: any;
 }
 
-interface OwnState {
-  preview: HTMLImageElement | void;
-}
+const CustomLazyImage = ({ src, ...props }: OwnProps) => (
+  <Img
+    offset={300}
+    fallback={() => <span {...props} style={{ backgroundColor: '#dedede', opacity: 0.2, ...props.style }} />}
+    src={src}
+    {...props}
+  />
+);
 
-class CustomImage extends React.Component<OwnProps, OwnState> {
-  static displayName = 'CustomImage';
-
-  state = {
-    preview: undefined,
-  };
-
-  componentDidMount() {
-    this.updateImagePreview();
-  }
-
-  componentDidUpdate(prevProps: OwnProps) {
-    if (prevProps.src !== this.props.src) {
-      this.setState(
-        {
-          preview: undefined,
-        },
-        this.updateImagePreview
-      );
-    }
-  }
-
-  updateImagePreview() {
-    const { src } = this.props;
-
-    const preview = new Image();
-    preview.src = src;
-
-    preview.onload = event => {
-      this.setState({
-        // @ts-ignore
-        preview: event.path && event.path[0] && event.path[0].src,
-      });
-    };
-  }
-
-  render() {
-    const { src, ...props } = this.props;
-    const { preview } = this.state;
-
-    if (!preview) return <span {...props} style={{ backgroundColor: '#dedede', opacity: 0.2, ...props.style }} />;
-
-    return <img {...props} src={preview} />;
-  }
-}
-
-export default CustomImage;
+export default CustomLazyImage;
--- src/app/shell/header/header-view.tsx
+++ src/app/shell/header/header-view.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Link } from 'react-router-dom';
 
 import { HOME, SEARCH } from '../../../constants/appRoutes';
+import CustomImage from '../../components/image';
 
 class HeaderView extends React.Component {
   static displayName = 'HeaderView';
@@ -9,8 +10,14 @@ class HeaderView extends React.Component {
   render() {
     return (
       <header className="Header">
-        <Link to={{ pathname: HOME }}>Home</Link>
-        <Link to={{ pathname: SEARCH.replace(':query', 'types=psychic,fighting') }}>Search</Link>
+        <Link className="Header-item" to={{ pathname: HOME }}>
+          <CustomImage src={require('../../../assets/images/pokeball.png')} className="Header-logo" />
+        </Link>
+        {false && (
+          <Link className="Header-item" to={{ pathname: SEARCH.replace(':query', 'types=psychic,fighting') }}>
+            Search
+          </Link>
+        )}
       </header>
     );
   }
