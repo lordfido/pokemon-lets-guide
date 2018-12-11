ace045d85a6e6fffca18041e9b89767c2e4de1dc
Rafael Pérez <rafael.perez@imperdiblesoft.com>
2018-12-11 03:13:00 +0100

    Add app link to footer

--- src/app/shell/footer/footer-view.tsx
+++ src/app/shell/footer/footer-view.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
 import { isProduction } from '../../../common/utils/platforms';
-
-import { APP_NAME } from '../../../constants/branding';
 import { languageISOs } from '../../utils/translations';
+
+import Link from '../../components/link';
+
+import { APP_NAME, APP_WEB } from '../../../constants/branding';
 
 const packageJson = require('../../../../package.json');
 const APP_VERSION = packageJson.version;
@@ -21,7 +23,14 @@ class FooterView extends React.Component<IOwnProps> {
       <footer className="Page Footer">
         <div className="Footer-wrapper">
           <p className="Footer-text">
-            {APP_NAME} {!isProduction() && `v${APP_VERSION}`} | {new Date().getFullYear()}{' '}
+            <Link
+              options={{
+                id: 'app-web',
+                label: APP_NAME,
+                to: APP_WEB,
+              }}
+            />{' '}
+            {!isProduction() && `v${APP_VERSION}`} | {new Date().getFullYear()}{' '}
           </p>
           <p className="Footer-text">
             {languageISOs.map((language, index) => (
