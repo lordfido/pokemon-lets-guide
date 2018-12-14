2c50cbd10b8c0512c488b22a209b1e94a77d0f16
Rafael Pérez <rafael.perez@imperdiblesoft.com>
2018-12-14 19:10:44 +0100

    MediaQueries have been updated

--- src/constants/styles-media-queries.ts
+++ src/constants/styles-media-queries.ts
@@ -1,4 +1,23 @@
-export const MOBILE_LARGE = '@media screen and (min-width: 480px)';
-export const TABLET_OR_LANDSCAPE = '@media screen and (orientation: landscape), screen and (min-width: 720px)';
+// Mobile
+const MOBILE = '@media screen and (min-width: 320px)';
+const MAX_MOBILE = '@media screen and (min-width: 479px)';
+export const MOBILE_L = '@media screen and (min-width: 480px)';
+export const MAX_MOBILE_L = '@media screen and (max-width: 511)';
+export const MOBILE_XL = '@media screen and (min-width: 512px)';
+const MAX_MOBILE_XL = '@media screen and (max-width: 719px)';
+
+// Tablet
 export const TABLET = '@media screen and (min-width: 720px)';
+export const MAX_TABLET = '@media screen and (max-width: 1023px)';
+
+// Desktop
 export const DESKTOP = '@media screen and (min-width: 1024px)';
+export const MAX_DESKTOP = '@media screen and (max-width: 1279px)';
+export const DESKTOP_L = '@media screen and (min-width: 1280px)';
+export const MAX_DESKTOP_L = '@media screen and (max-width: 1920px)';
+
+// Bigger screens
+export const HD_DISPLAY = '@media screen and (min-width: 1921px)';
+
+// Mixes
+export const TABLET_OR_LANDSCAPE = '@media screen and (orientation: landscape), screen and (min-width: 720px)';
