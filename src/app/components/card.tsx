2f5b7b12aca358c4839000065b992f0e36d90349
Rafael Pérez <rafael.perez@imperdiblesoft.com>
2018-12-14 16:07:40 +0100

    Card component has been migrated

--- src/app/components/card.tsx
+++ src/app/components/card.tsx
@@ -1,21 +1,54 @@
 import classnames from 'classnames';
 import * as React from 'react';
+import injectSheet from 'react-jss';
 
 import CustomImage from './image';
 
+import { AVATAR_SIZE, BORDER_RADIUS, SIZE_S, SIZE_XXS } from '../../constants/styles';
+
+import { ISheet } from '../root.models';
+
+const sheet: ISheet = {
+  picture: {
+    display: 'block',
+    margin: '0 auto',
+    marginTop: SIZE_XXS,
+    maxWidth: '100%',
+    minHeight: AVATAR_SIZE,
+    minWidth: AVATAR_SIZE,
+  },
+  title: {},
+  wrapper: {
+    backgroundColor: '#fff',
+    borderRadius: BORDER_RADIUS,
+    display: 'inline-block',
+    maxWidth: '100%',
+    overflow: 'hidden',
+    padding: SIZE_XXS,
+    textAlign: 'center',
+
+    '@media screen and (min-width: 720px)': {
+      padding: SIZE_S,
+    },
+  },
+};
+
 interface IOwnProps {
   children: JSX.Element;
+  classes: { [key: string]: string };
   className?: string;
   title?: string;
   image?: string;
 }
 
-const Card = ({ children, className, image, title }: IOwnProps) => (
-  <div className={classnames('Card', className)}>
-    {!!title && <span className="Card-title">{title}</span>}
-    {!!image && <CustomImage className="Card-image" src={image} />}
+const unstyledCard = ({ children, classes, className, image, title }: IOwnProps) => (
+  <div className={classnames(classes.wrapper, className)}>
+    {!!title && <span className={classes.title}>{title}</span>}
+    {!!image && <CustomImage className={classes.picture} src={image} />}
     {children}
   </div>
 );
+
+const Card = injectSheet(sheet)(unstyledCard);
 
 export default Card;
